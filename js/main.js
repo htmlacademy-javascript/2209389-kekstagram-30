f6bc3816861873b3c86a8efc5b0b4f14e8c39661
Никита Погорелов <nikipog@mail.ru>
2023-10-18 19:50:54 +0300

    созданы функции и массивы. не решена проблема дублируемых комментариев

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 const COMMENT_ID = createRandomIdFromRange(1, 6);
 const PHOTO_ID = createRandomIdFromRange (1, 25);
 const PICTURE_ID = createRandomIdFromRange(1, 25);
-const LIKES_ID = createRandomIdFromRange(15, 200)
+const LIKES_ID = createRandomIdFromRange(15, 200);
 const DESCRIPTION = [
-'Я эгоист, но при этот ответственный. Поэтому я не совершаю того, из-за чего будет кому-то плохо.',
-'Кто-то уйдет и бросит тебя, а кто-то встанет рядом и будет прикрывать от пуль. ',
-'Я так мало цепляюсь за жизнь, что совсем не боюсь смерти.',
-'Если ты одинок, то это не ровно тому, что ты слабый. Только сильный человек может ждать того, кто заслуживает.',
-'Есть такое понятие, как жить дальше. Попробуй, возможно, получится.',
-'Я очень хороший человек, жаль моей жене не всегда это нравится.',
-'Не важно, чем я занят в данный момент, в любом моем деле присутствует дух соперничества.',
-'Я пришел из ниоткуда, поэтому не боюсь пропасть в никуда.',
-'Думай не о том, что я сказал, а о том, о чем решил промолчать.',
-'Даже если у вас есть голова, это совсем не означает наличие в ней мозга.',
+  'Я эгоист, но при этот ответственный. Поэтому я не совершаю того, из-за чего будет кому-то плохо.',
+  'Кто-то уйдет и бросит тебя, а кто-то встанет рядом и будет прикрывать от пуль. ',
+  'Я так мало цепляюсь за жизнь, что совсем не боюсь смерти.',
+  'Если ты одинок, то это не ровно тому, что ты слабый. Только сильный человек может ждать того, кто заслуживает.',
+  'Есть такое понятие, как жить дальше. Попробуй, возможно, получится.',
+  'Я очень хороший человек, жаль моей жене не всегда это нравится.',
+  'Не важно, чем я занят в данный момент, в любом моем деле присутствует дух соперничества.',
+  'Я пришел из ниоткуда, поэтому не боюсь пропасть в никуда.',
+  'Думай не о том, что я сказал, а о том, о чем решил промолчать.',
+  'Даже если у вас есть голова, это совсем не означает наличие в ней мозга.',
 ];
 const MESSAGES = [
-'Всё отлично!',
-'В целом всё неплохо. Но не всё.',
-'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
 const NAMES = ['Bilfinger', 'Borzel', 'Gavein', 'Zenkrebe', 'Noskov', 'Synevite'];
@@ -31,7 +31,7 @@ const getRandomInteger = (min, max) => {
   const result = Math.random() * (upper - lower + 1) + lower;
 
   return Math.floor(result);
-}
+};
 
 function createIdGenerator () {
   let lastGeneratedId = 0;
@@ -49,76 +49,34 @@ function createRandomIdFromRange (min, max) {
   return function () {
     let currentValue = getRandomInteger(min, max); //получаем случайное целое число
     if (previousValues.length >= (max - min + 1)) {
-      console.error('Перебраны все числа из диапазона от ' + min + ' до ' + max);
-     return null;
+      console.error(`Перебраны все числа из диапазона от ${ min } до ${ max}`);
+      return null;
     }
 
-    while (previousValues.includes(currentValue)) {  //проверяем на уникальность (includes возвращает true false)
-      currentValue = getRandomInteger(min, max)
+    while (previousValues.includes(currentValue)) { //проверяем на уникальность (includes возвращает true false)
+      currentValue = getRandomInteger(min, max);
     }
-     previousValues.push(currentValue); //добавляем полученное случайное число в массив-хранилище идентификаторов
+    previousValues.push(currentValue); //добавляем полученное случайное число в массив-хранилище идентификаторов
 
-  return currentValue //возвращаем случайное целое число
+    return currentValue; //возвращаем случайное целое число
   };
 }
 
-const getRandomArrayElement = (elements) => {
-return elements[getRandomInteger(0, elements.length - 1)]
-}
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-const createComment = () => {
-  return {
-id: generateCommentId (),
-avatar: `img/avatar-${COMMENT_ID()}.svg`,
-message: getRandomArrayElement(MESSAGES),
-name: getRandomArrayElement(NAMES),
-  };
-};
+const createComment = () => ({
+  id: generateCommentId (),
+  avatar: `img/avatar-${COMMENT_ID()}.svg`,
+  message: getRandomArrayElement(MESSAGES),
+  name: getRandomArrayElement(NAMES),
+});
 
-const createPhotoDescription = () => {
-return {
-id: PHOTO_ID(),
-url: `photos/${PICTURE_ID()}.jpg`,
-description: getRandomArrayElement(DESCRIPTION),
-likes: LIKES_ID(),
-}
+const createPhotoDescription = () => ({
+  id: PHOTO_ID(),
+  url: `photos/${PICTURE_ID()}.jpg`,
+  description: getRandomArrayElement(DESCRIPTION),
+  likes: LIKES_ID(),
+  comments: createComment(),
+});
 
-};
-
-
-
-
-//функция, создающая объект - описание фотографии, опубликованной пользователем.
-const createDescription = () => {
-  return
-  id: '',
-  url: '',
-  description: '',
-  likes: '';
-  comments: [{
-    id: '',
-  avatar: 'img/avatar-6.svg',
-  message: 'В целом всё неплохо. Но не всё.',
-  name: 'Артём',
-  }, {}]
-}
-
-//функции для создания массива
-//массив из 25 сгенерированных объектов
-//объект 1 из 25 - описание фотографии
-//объект состоит {
-  id: число от 1 до 25 (Number)
-  url: строка внутри нее число от 1 до 25 (String)
-  description: содержит описание (string)
-  likes: число лайков от  15 до 200 (number)
-  comments: [
-    вложенный в массив объект comments {
-      id: любое число (number)
-      avatar: img/avatar-(случайное число от 1-6).svg (string)
-      message: выбрать 1 или 2 случайных предложения (string)
-      name: имя придумай 6 любых (string)
-    }
-  ]
-
-}
-
+const photoDescription = Array.from({length: 25}, createPhotoDescription);
