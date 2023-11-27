d5d617d571b3887a1590478184e7e740b3225413
Никита Погорелов <nikipog@mail.ru>
2023-11-27 23:57:53 +0300

    доработан сброс масштаба и эффектов, массивы названы во мн.числе, переименован обработчик, изменено название и перенесены константы

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 // import { getPictures } from './data.js';
 import { renderGallery } from './gallery.js';
-import { showImageEditorForm, setImageUploadFormSubmit, hideImageEditorForm } from './form.js';
+import { showImageEditorForm, setImageUploadFormSubmit, onImageUploadCancelButtonClick } from './form.js';
 import './server.js';
 import { getData } from './server.js';
-import { initFilter } from './filters.js';
+import { initializeFilter } from './filters.js';
 
 
 getData()
   .then ((pictures) => {
     renderGallery(pictures);
-    initFilter(pictures);
+    initializeFilter(pictures);
   });
 
 showImageEditorForm();
 
 
-setImageUploadFormSubmit(hideImageEditorForm);
+setImageUploadFormSubmit(onImageUploadCancelButtonClick);
 
